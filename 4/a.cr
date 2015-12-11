0e7a9313b170acebb237d2b7d946263d84559761
Oleh Prypin <blaxpirit@gmail.com>
2015-12-12 01:34:49 +0200

    4

---
+++ 4/a.cr
@@ -0,0 +1,12 @@
+require "crypto/md5"
+
+inp = File.read("input.txt").strip
+
+i = 0
+loop do
+  i += 1
+  hash = Crypto::MD5.hex_digest("#{inp}#{i}")
+  break if hash.starts_with? "00000"
+end
+
+puts i
