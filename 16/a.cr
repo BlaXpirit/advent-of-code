57b9c08080624abed5b9c733ec9c0c003ab4f530
Oleh Prypin <blaxpirit@gmail.com>
2015-12-25 22:08:57 +0200

    16

---
+++ 16/a.cr
@@ -0,0 +1,15 @@
+def extract(s)
+  s.scan(/(\w+): (\d+)/).map { |m|
+    {m[1], m[2].to_i }
+  } .to_h
+end
+
+sample = extract File.read("sample.txt")
+
+File.each_line("input.txt") do |line|
+  candidate = extract line
+  if sample == sample.merge candidate
+    line =~ /(\d+)/
+    puts $1
+  end
+end
